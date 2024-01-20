5cf8cd4419ac3c8f7b6d18fde4918e5ef118c0f4
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-01-20 09:06:32 +0000

    [Add]deviseの認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #顧客用
+  devise_for :endusers, skip: [:passwords], controller: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  #管理者用
+  devise_for :admins, skip: [:registrations, :passswords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
