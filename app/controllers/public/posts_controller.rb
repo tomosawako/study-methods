0175e487113dc756dcffbde86c0c2bdd5270ea73
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-01-21 13:23:14 +0000

    [Start]投稿機能

---
+++ app/controllers/public/posts_controller.rb
@@ -0,0 +1,14 @@
+class Public::PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @post = Post.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+    get 'posts/new'
+  end
+  scope module: 'public' do
+  root to: 'homes#top'
+  get '/about' => "homes#about", as: 'about'
+  end
+
   #顧客用
   devise_for :endusers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
