170ab964ade99bd6baaa50ac85e476608b513c46
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-01-26 02:39:48 +0000

    [Update]バリデーション機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -28,9 +28,12 @@ class Public::PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post_path(post.id)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
