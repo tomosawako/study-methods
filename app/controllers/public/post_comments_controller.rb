e2adc59c4e38ee1c952c18da93bef13e3ef317cf
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-01-27 03:48:45 +0000

    [Add]コメント機能

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,16 +3,23 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_enduser.post_comments.new(post_comments_params)
+    comment = PostComment.new(post_comment_params)
+    comment.enduser_id = current_enduser.id
     comment.post_id = post.id
     comment.save
     redirect_to post_path(post)
   end
 
+  def destroy
+    comment = PostComment.find(params[:id])
+    comment.destroy
+    redirect_to post_path(comment.post_id)
+  end
+
   private
 
-  def post_comments_params
-    params.require(:post).permit(:comment)
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
   end
 
 end
