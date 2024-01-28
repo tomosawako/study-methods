302db96e0ce851ed7840e199852e8798c69d7b51
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-01-28 13:46:22 +0000

    [Start]ブックマーク機能

--- app/models/enduser.rb
+++ app/models/enduser.rb
@@ -1,6 +1,7 @@
 class Enduser < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
