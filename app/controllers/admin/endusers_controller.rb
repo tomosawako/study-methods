b21f3eebe127151211f9884fa9b8c220d151cb96
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-01-30 08:17:39 +0000

    [Update]会員管理機能(編集画面)

--- app/controllers/admin/endusers_controller.rb
+++ app/controllers/admin/endusers_controller.rb
@@ -7,10 +7,23 @@ class Admin::EndusersController < ApplicationController
 
   def show
     @enduser = Enduser.find(params[:id])
-    #@posts = @enduser.posts.page(params[:page])
+    @posts = @enduser.posts.page(params[:page])
   end
 
   def edit
     @enduser = Enduser.find(params[:id])
   end
+
+  def update
+    enduser = Enduser.find(params[:id])
+    enduser.update(enduser_params)
+    redirect_to admin_enduser_path(enduser)
+  end
+
+  private
+
+  def enduser_params
+    params.require(:enduser).permit(:name, :email, :profile_image, :is_active)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'posts/show'
+    get 'posts/edit'
+  end
   #顧客用
   devise_for :endusers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -24,6 +28,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories, only: [:index, :create, :edit, :update]
     resources :endusers, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:show, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
