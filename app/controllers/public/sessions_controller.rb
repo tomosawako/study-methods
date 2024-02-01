f13141081280991bcd3ea4e1352ded138ee1a30c
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-02-01 13:59:01 +0000

    [Finish]会員管理機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :enduser_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -26,5 +27,18 @@ class Public::SessionsController < Devise::SessionsController
   # end
   def after_sign_in_path_for(resource)
     posts_path
+  end
+
+  private
+
+  # エンドユーザーがアクティブであるかを判断するメソッド
+  def enduser_state
+    enduser = Enduser.find_by(email: params[:enduser][:email])
+    return if enduser.nil?
+    return unless enduser.valid_password?(params[:enduser][:password])
+    return if enduser.is_active == true
+    if enduser.is_active == false
+      redirect_to new_enduser_registration_path
+    end
   end
 end
