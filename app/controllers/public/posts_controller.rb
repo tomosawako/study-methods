fc4d1b6eb8f7de7ae16801bce9482c874d74cc32
tomosawako <145893964+tomosawako@users.noreply.github.com>
2024-02-27 07:50:04 +0000

    [Update]投稿編集のバグ修正

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -44,9 +44,9 @@ class Public::PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    if post.update(post_params)
-      redirect_to post_path(post.id)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
     else
       render :edit
     end
